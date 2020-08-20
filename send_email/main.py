200ab72a904eea74c2bb9ad68953aab671645ee1
meghaldonde <meghal.donde@gmail.com>
2020-08-19 23:37:18 -0400

    Initial commit

---
+++ send_email/main.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Meghal Donde'
+email['to'] = 'meghaldeveloper@gmail.com'
+email['subject'] = 'cool program'
+
+email.set_content(html.substitute({'name': 'Meghal'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.login('meghaltests@gmail.com', 'Abcd_1000')
+  smtp.send_message(email)
+  print('Sent email successfully!')
